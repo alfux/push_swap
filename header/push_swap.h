8eaa606964625c2791c3f2167a31f14c917d6528
alfux <afuchs@student.42mulhouse.fr>
2022-05-18 16:18:46 +0200

    save180520221618

--- header/push_swap.h
+++ header/push_swap.h
@@ -6,11 +6,18 @@
 /*   By: afuchs <alexis.t.fuchs@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 13:49:14 by afuchs            #+#    #+#             */
-/*   Updated: 2022/05/18 13:50:25 by afuchs           ###   ########.fr       */
+/*   Updated: 2022/05/18 16:17:55 by afuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft.h"
+
+struct					s_stack
+{
+	t_list	*frst;
+	t_list	*last;
+};
+typedef struct s_stack	t_stack;
 
 #endif
