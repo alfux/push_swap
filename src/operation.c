4547b605885cd2db8a9e5531685a2a5ed9e5d93d
alfux <afuchs@student.42mulhouse.fr>
2022-05-19 21:33:51 +0200

    save190520222133

--- src/operation.c
+++ src/operation.c
@@ -6,24 +6,45 @@
 /*   By: afuchs <alexis.t.fuchs@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 13:41:54 by afuchs            #+#    #+#             */
-/*   Updated: 2022/05/19 14:25:21 by afuchs           ###   ########.fr       */
+/*   Updated: 2022/05/19 21:32:53 by afuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 char	swpstk(t_stk *stk)
 {
-	t_list	*buf;
+	void	*buf;
 
-	if (stk->frst && stk->frst->next)
+	if (stk && stk->frst && stk->frst->next)
 	{
-		buf = stk->frst->next;
-		stk->frst->next = buf->next;
-		if (!buf->next)
-			stk->last = stk->frst;
-		buf->next = stk->frst;
-		stk->frst = buf;
+		buf = stk->frst->content;
+		stk->frst->content = stk->frst->next->content;
+		stk->frst->next->content = buf;
 		return (1);
 	}
 	return (0);
+}
+
+char	pshstk(t_stk *A, t_stk *B)
+{
+	t_list	*buf;
+
+	if (A && B && A->frst)
+	{
+		buf = A->frst;
+		A->frst = A->frst->next;
+		buf->next = B->frst;
+		B->frst = buf;
+		if (!B->last)
+			B->last = B->frst;
+		if (!A->frst)
+			A->last = (void *)0;
+		return (1);
+	}
+	return (0);
+}
+
+char	rotstk(t_stk stk)
+{
+	//A SUIVRE
 }
